c99e2e9c8119be8f393357f0691a43b2e9504ac4
ImJulyBorn <106574769+ImJulyBorn@users.noreply.github.com>
2022-09-14 15:07:57 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,24 @@
+var status = "";
+function preload(){
+    img = loadImage("desk.jpeg")
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects"
+}
+function modelLoaded(){
+    console.log("model is loaded .");
+    status = true;
+    ObjectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    } 
+    console.log(results);
+}
+function back(){
+    window.location = "index.html";
+}
